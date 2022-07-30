9c763fc89d9226a708d676b64a30f4b172defcae
Maria Rodriguez <mary_cherny@msn.com>
2022-07-30 09:38:42 +0200

    Ex1

---
+++ src/componentes/Naves/Styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const NavesStyle = styled.div `
+
+    ul{
+        justify-content: center;
+    }
+    li{
+        margin: 0 20% 2% 20%;
+        background-color: #1a1919;
+        list-style-type: none; 
+        padding: 1px 0 1px 1rem;     
+    }
+    p{
+        margin: none;
+        font-size: 14px;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-weight: lighter;
+        
+    }
+    h4{
+        font-size: 16px;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-weight: lighter;
+    }
+ 
+`;
